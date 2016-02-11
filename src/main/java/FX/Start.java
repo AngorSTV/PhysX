2d24275d97ca35022cbeea2f8284cf94ee76faf6
Angor <angor@inbox.ru>
2016-02-11 14:05:52 +0300

    init FX version

---
+++ src/main/java/FX/Start.java
@@ -0,0 +1,18 @@
+package FX;/**
+ * Created by Андрей on 11.02.2016.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Start extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
